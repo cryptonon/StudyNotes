d47955577cc2952ab007bcfc7c16fbfc19170af3
cryptonon <aayushphuyal0074@gmail.com>
2020-07-13 16:17:53 -0400

    Initial Commit

---
+++ StudyNotes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StudyNotes
+//
+//  Created by Aayush Mani Phuyal on 7/13/20.
+//  Copyright © 2020 Aayush Phuyal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
