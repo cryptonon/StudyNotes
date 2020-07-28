7f06e4313d22a80a9b47b8cd41eb2360b4a6da5f
cryptonon <aayushphuyal0074@gmail.com>
2020-07-28 17:55:03 -0400

    Fix for displaying Color Gradient correctly after Screen Rotations

--- StudyNotes/Views/HomeCollectionCell.m
+++ StudyNotes/Views/HomeCollectionCell.m
@@ -12,30 +12,36 @@
 
 // MARK: Properties
 @property (weak, nonatomic) IBOutlet UIImageView *cellPosterView;
+@property (strong, nonatomic) UIView *gradientView;
 
 @end
 
 @implementation HomeCollectionCell
+
+// Method that lays out subviews
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    [self.gradientView removeFromSuperview];
+    self.gradientView = [[UIView alloc] initWithFrame: self.contentView.frame];
+    [self configureColorGradient];
+}
 
 // Setter method that sets Poster of cell when indexPath is provided
 - (void)setIndexPath:(NSIndexPath *)indexPath {
     NSString *imageName = [NSString stringWithFormat:@"Cell%li", indexPath.item];
     self.cellPosterView.image = [UIImage imageNamed:imageName];
     self.cellPosterView.alpha = 0.875;
-    [self configureColorGradient];
 }
 
 // Method that configures color gradient over cell images
 - (void)configureColorGradient {
-    [self layoutIfNeeded];
-    UIView *gradientView = [[UIView alloc] initWithFrame: self.contentView.frame];
     CAGradientLayer *gradientLayer = [[CAGradientLayer alloc] init];
-    gradientLayer.frame = gradientView.frame;
+    gradientLayer.frame = self.gradientView.frame;
     gradientLayer.colors = @[ (id)[[UIColor clearColor] CGColor], (id)[[UIColor blackColor] CGColor] ];
     gradientLayer.locations = @[@0.0, @1.0];
-    [gradientView.layer insertSublayer:gradientLayer atIndex:0];
-    [self.cellPosterView addSubview:gradientView];
-    [self.cellPosterView bringSubviewToFront:gradientView];
+    [self.gradientView.layer insertSublayer:gradientLayer atIndex:0];
+    [self.cellPosterView addSubview:self.gradientView];
+    [self.cellPosterView bringSubviewToFront:self.gradientView];
 }
 
 @end
